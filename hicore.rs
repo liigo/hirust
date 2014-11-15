9fea3af01abe9ea0bf2daea4a42972480653f871
liigo <com.liigo@gmail.com>
2014-11-15 09:10:54 +0800

    use `core::str::StrPrelude::as_ptr()` to simplify code, no longer use transmute. use `panic_fmt` instead of old `fail_fmt` lang item. (compiled with the latest nightly rustc.)

--- hicore.rs
+++ hicore.rs
@@ -5,14 +5,12 @@ extern crate libc;
 extern crate core;
 
 use libc::puts;
-use core::intrinsics::transmute;
+use core::str::StrPrelude; // &str::as_prt()
 
 #[start]
 fn start(_argc: int, _argv: *const *const u8) -> int {
-	let s = "Hi Rust! (uses core crate)\0"; // &str
     unsafe {
-    	let (s,_): (*const i8, uint) = transmute(s); // see core::raw::Slice
-    	puts(s);
+    	puts("Hi Rust! (uses core crate)\0".as_ptr() as *const i8);
     }
 	return 0;
 }
@@ -20,8 +18,8 @@ fn start(_argc: int, _argv: *const *const u8) -> int {
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 
-#[lang = "fail_fmt"]
-extern fn fail_fmt(_args: &core::fmt::Arguments,
+#[lang = "panic_fmt"]
+extern fn panic_fmt(_args: &core::fmt::Arguments,
                    _file: &str,
                    _line: uint) -> ! {
     loop {}
